ae0a03d4b4e5fbcd52f5c34c24d992387ed34086
Serhii Tochynskyi <tochynskyi.s@gmail.com>
2022-08-03 18:48:54 +0300

    fix bags

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Article } from "./components/pages/Article";
 import { Home } from "./components/pages/Home";
+import { store } from "./store";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/article" element={<Article />} />
-    </Routes>
+    <Provider store={store}>
+      <BrowserRouter basename="/test-task">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/article" element={<Article />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
